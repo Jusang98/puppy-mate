e110d3921657059286be748d26a600bf35df02ec
devGO <been2zz@naver.com>
2025-04-11 15:27:13 +0900

    test

--- puppy-mate/app/map/page.tsx
+++ puppy-mate/app/map/page.tsx
@@ -3,9 +3,31 @@
 import { Map, CustomOverlayMap } from 'react-kakao-maps-sdk';
 import useKakaoLoader from './components/use-kakao-loader';
 import { useCurrentLocation } from './hooks/useCurrentLocation';
+import { useEffect, useState } from 'react';
 export default function BasicMap() {
   useKakaoLoader();
-  const { location, error } = useCurrentLocation();
+  const [course, setCourse] = useState<{ lat: number; lng: number }[]>([]);
+  const { location } = useCurrentLocation();
+
+  useEffect(
+    () => {
+      const interval = setInterval(() => {
+        if (location) {
+          setCourse(prevCourse => [...prevCourse, location]);
+        }
+      }, 5000);
+
+      return () => clearInterval(interval);
+    },
+    [location]
+  );
+  useEffect(
+    () => {
+      console.log(course);
+    },
+    [course]
+  );
+
   return (
     <Map // 지도를 표시할 Container
       id="map"
@@ -20,8 +42,8 @@ export default function BasicMap() {
       <CustomOverlayMap position={location || { lat: 37.5665, lng: 126.978 }}>
         <div
           style={{
-            width: '10px',
-            height: '10px',
+            width: '20px',
+            height: '20px',
             backgroundColor: 'red',
             borderRadius: '50%',
             border: '1px solid #000'

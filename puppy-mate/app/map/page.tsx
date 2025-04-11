bb3c5fe17cb93e299da76136877deda445604ca3
devGO <been2zz@naver.com>
2025-04-11 15:17:30 +0900

    style: 마커 스타일 변경경

--- puppy-mate/app/map/page.tsx
+++ puppy-mate/app/map/page.tsx
@@ -1,29 +1,33 @@
-'use client'
+'use client';
 
-import { Map, CustomOverlayMap } from "react-kakao-maps-sdk"
-import useKakaoLoader from "./components/use-kakao-loader"
-import { useCurrentLocation } from "./hooks/useCurrentLocation"
+import { Map, CustomOverlayMap } from 'react-kakao-maps-sdk';
+import useKakaoLoader from './components/use-kakao-loader';
+import { useCurrentLocation } from './hooks/useCurrentLocation';
 export default function BasicMap() {
-  useKakaoLoader()
-  const { location, error } = useCurrentLocation()
+  useKakaoLoader();
+  const { location, error } = useCurrentLocation();
   return (
     <Map // 지도를 표시할 Container
       id="map"
-      center={location || { lat: 37.5665, lng: 126.9780 }} 
+      center={location || { lat: 37.5665, lng: 126.978 }}
       style={{
         // 지도의 크기
-        width: "100%",
-        height: "350px",
+        width: '100%',
+        height: '350px'
       }}
       level={3} // 지도의 확대 레벨
     >
-      <CustomOverlayMap position={location || { lat: 37.5665, lng: 126.9780 }}>
+      <CustomOverlayMap position={location || { lat: 37.5665, lng: 126.978 }}>
         <div
-          style={{padding:"42px", backgroundColor:"#fff", color:"#000"}}
-        >
-          Custom Overlay!
-        </div>
+          style={{
+            width: '10px',
+            height: '10px',
+            backgroundColor: 'red',
+            borderRadius: '50%',
+            border: '1px solid #000'
+          }}
+        />
       </CustomOverlayMap>
     </Map>
-  )
+  );
 }
--- puppy-mate/app/page.tsx
+++ puppy-mate/app/page.tsx
@@ -1,22 +1,4 @@
-// 'use client'
-
-// import { Map } from "react-kakao-maps-sdk"
-// import { useKakaoLoader as useKakaoLoaderOrigin } from "react-kakao-maps-sdk"
-
-// export default function BasicMap() {
-//   const [loading, error] = useKakaoLoaderOrigin({
-//     appkey: process.env.NEXT_PUBLIC_KAKAO_KEY || "default-app-key",
-//     libraries: ["clusterer", "drawing", "services"],
-//   })
-
-//   if (!loading) return <div>지도를 불러오는 중...</div>
-//   if (error) return <div>지도를 불러오는 데 실패했습니다.</div>
-
-//   return (
-//     <Map
-//       center={{ lat: 33.5563, lng: 126.79581 }}
-//       style={{ width: "100%", height: "360px" }}
-//       level={3}
-//     />
-//   )
-// }
+'use client';
+export default function Page() {
+  return <div>main</div>;
+}

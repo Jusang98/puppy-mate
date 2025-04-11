2b02b04d5900df5f3b83aba8675129a86f911232
devGO <been2zz@naver.com>
2025-04-11 12:11:19 +0900

    fix: 불필요한 Script 제거

--- puppy-mate/app/map/layout.tsx
+++ puppy-mate/app/map/layout.tsx
@@ -1,14 +1,8 @@
-import Script from 'next/script'
 import React from 'react'
-export const API = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_KEY}&libraries=services,clusterer&autoload=false`
 
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {/* <Script
-        src={API}
-        strategy="beforeInteractive"
-      /> */}
       {children}
     </>
   )

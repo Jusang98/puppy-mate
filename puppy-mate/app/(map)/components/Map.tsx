b5d620a922141618df886c1a56abd15d0d6e5dcc
jwanp <joohwan726@naver.com>
2025-04-15 17:55:47 +0900

    feat: 경로 저장 기능 - kakaoMap 현재위치 기준 가운데로 정렬 ref 이용

--- puppy-mate/app/(map)/components/Map.tsx
+++ puppy-mate/app/(map)/components/Map.tsx
@@ -2,15 +2,13 @@
 import { Map as KakaoMap, MapMarker, Polyline } from 'react-kakao-maps-sdk';
 import useKakaoLoader from '../lib/use-kakao-loader';
 import { useCurrentLocation } from '../hooks/useCurrentLocation';
-import { use, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import useMapStore from '@/store/useMapStore';
 import { getDistance } from '../utils/getDistance';
 export function Map() {
+  const mapRef = useRef<kakao.maps.Map | null>(null);
   const { location, error } = useCurrentLocation();
-  const [mapCenter, setMapCenter] = useState<{ lat: number; lng: number }>({
-    lat: location?.lat ?? 37.5665,
-    lng: location?.lng ?? 126.978, // 기본 위치 (서울)
-  });
+
   const path = useMapStore((state) => state.path); // Zustand에서 경로 가져오기
   const addPathPoint = useMapStore((state) => state.addPathPoint); // Zustand에서 경로 추가 함수 가져오기
   const isSavingPath = useMapStore((state) => state.isSavingPath); // Zustand에서 경로 저장 여부 가져오기
@@ -18,20 +16,20 @@ export function Map() {
   useKakaoLoader();
 
   useEffect(() => {
-    if(location){
-      setMapCenter({
-        lat: location?.lat,
-        lng: location?.lng,
-      });
+    if (!mapRef.current) return;
+
+    if (location) {
+      const center = new kakao.maps.LatLng(location.lat, location.lng);
+      mapRef.current.setCenter(center);
     }
+
     if (isSavingPath && location) {
       const lastPosition = path.at(-1);
       if (!lastPosition || getDistance(lastPosition, location) > 1) {
         addPathPoint(location); // 경로 저장
-        console.log('경로 저장:', path);
       }
-    } 
-  }, [location]);
+    }
+  }, [location, isSavingPath]);
 
   //watchposition하고 clearpostion 해줘야함
 
@@ -41,12 +39,11 @@ export function Map() {
 
   return (
     <>
-      <button onClick={toggleSavingPath}>
-        토글 버튼{isSavingPath ? 'on' : 'off'}
-      </button>
+      <button onClick={toggleSavingPath}>토글 버튼{isSavingPath ? 'on' : 'off'}</button>
       <KakaoMap
-        id='map'
-        center={mapCenter}
+        id="map"
+        ref={mapRef}
+        center={{ lat: 37.566535, lng: 126.977125 }}
         style={{
           width: '100%',
           height: '350px',
@@ -63,9 +60,9 @@ export function Map() {
               <Polyline
                 path={path} // 폴리라인 경로
                 strokeWeight={5} // 선 두께
-                strokeColor='#FF0000' // 선 색상
+                strokeColor="#FF0000" // 선 색상
                 strokeOpacity={0.8} // 선 투명도
-                strokeStyle='solid' // 선 스타일
+                strokeStyle="solid" // 선 스타일
               />
             )}
           </>

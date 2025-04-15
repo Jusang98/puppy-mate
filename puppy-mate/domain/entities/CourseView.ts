6f83cc3c8f00111f280e018075a8c7101bccd3f9
Jusang98 <rlatkdwn3379@gmail.com>
2025-04-15 10:31:33 +0900

    feat: courseView 엔티티 수정, create 기능 추가

--- puppy-mate/domain/entities/CourseView.ts
+++ puppy-mate/domain/entities/CourseView.ts
@@ -1,7 +1,19 @@
 import { Course } from './Course';
 
 export class CourseView extends Course {
-    constructor(public coordinates: { lat: number; lng: number }[]) {
-        super();
+    constructor(
+        id: number,
+        userId: number,
+        name: string,
+        courseImageUrl: string,
+        address: string,
+        isPublic: boolean,
+        distance: number,
+        duration: number,
+        createdAt: Date,
+        updatedAt: Date,
+        public coordinates: { lat: number; lng: number }[] // 좌표 데이터
+    ) {
+        super(id, userId, name, courseImageUrl, address, isPublic, distance, duration, createdAt, updatedAt);
     }
 }

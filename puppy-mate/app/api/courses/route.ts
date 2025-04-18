887874b2c0049bd90c116bc95e0014905c0a82c4
devGO <been2zz@naver.com>
2025-04-18 17:00:46 +0900

    feat: courses isPublic 가져오는 api 생성

--- puppy-mate/app/api/courses/route.ts
+++ puppy-mate/app/api/courses/route.ts
@@ -1,32 +1,20 @@
 import { CourseListDto } from '@/application/usecases/course/dto/CourseListDto';
-import CourseListUsecase from '@/application/usecases/course/CourseListUsecase';
 import { SbCourseRepository } from '@/infra/repositories/supabase/SbCourseRepository';
 import { NextResponse, NextRequest } from 'next/server';
 import CreateCourseUsecase from '@/application/usecases/course/CreateCourseUsecase';
 import { SbCoordinatesRepository } from '@/infra/repositories/supabase/SbCoordinatesRepository';
 import { CreateCourseDto } from '@/application/usecases/course/dto/CreateCourseDto';
+import CourseListIsPublicUsecase from '@/application/usecases/course/CourseListIsPublicUsecase';
+import { CourseListIsPublicDto } from '@/application/usecases/course/dto/CourseListIsPublicDto';
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
-    const searchParams = request.nextUrl.searchParams;
-    const southWestLat = searchParams.get('southWestLat');
-    const southWestLng = searchParams.get('southWestLng');
-    const northEastLat = searchParams.get('northEastLat');
-    const northEastLng = searchParams.get('northEastLng');
-
-    if (!southWestLat || !southWestLng || !northEastLat || !northEastLng) {
-      return NextResponse.json({ error: 'Invalid request' }, { status: 422 });
-    }
-
-    const courseListUsecase = new CourseListUsecase(new SbCourseRepository());
-
-    const courseListDto: CourseListDto = await courseListUsecase.execute(
-      parseFloat(southWestLat),
-      parseFloat(southWestLng),
-      parseFloat(northEastLat),
-      parseFloat(northEastLng)
+    const courseListUsecase = new CourseListIsPublicUsecase(
+      new SbCourseRepository()
     );
-    return NextResponse.json(courseListDto);
+
+    const courseListIsPublicDto: CourseListIsPublicDto[] = await courseListUsecase.execute();
+    return NextResponse.json(courseListIsPublicDto);
   } catch (error) {
     console.log('Error fetching course list:', error);
     return NextResponse.json(
@@ -35,6 +23,7 @@ export async function GET(request: NextRequest) {
     );
   }
 }
+
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json();
@@ -68,3 +57,33 @@ export async function POST(request: NextRequest) {
     );
   }
 }
+
+// export async function GET(request: NextRequest) {
+//   try {
+//     const searchParams = request.nextUrl.searchParams;
+//     const southWestLat = searchParams.get('southWestLat');
+//     const southWestLng = searchParams.get('southWestLng');
+//     const northEastLat = searchParams.get('northEastLat');
+//     const northEastLng = searchParams.get('northEastLng');
+
+//     if (!southWestLat || !southWestLng || !northEastLat || !northEastLng) {
+//       return NextResponse.json({ error: 'Invalid request' }, { status: 422 });
+//     }
+
+//     const courseListUsecase = new CourseListUsecase(new SbCourseRepository());
+
+//     const courseListDto: CourseListDto = await courseListUsecase.execute(
+//       parseFloat(southWestLat),
+//       parseFloat(southWestLng),
+//       parseFloat(northEastLat),
+//       parseFloat(northEastLng)
+//     );
+//     return NextResponse.json(courseListDto);
+//   } catch (error) {
+//     console.log('Error fetching course list:', error);
+//     return NextResponse.json(
+//       { error: 'Internal Server Error' },
+//       { status: 500 }
+//     );
+//   }
+// }

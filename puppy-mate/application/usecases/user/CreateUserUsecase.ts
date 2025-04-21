725080e248217ae608083a19d24df3f9b4593d6e
Jusang98 <rlatkdwn3379@gmail.com>
2025-04-21 22:38:23 +0900

    refactor: SbUserRepository에서 하던 해싱 작업 CreateUserService로 이동

--- puppy-mate/application/usecases/user/CreateUserUsecase.ts
+++ puppy-mate/application/usecases/user/CreateUserUsecase.ts
@@ -1,18 +1,38 @@
 import { UserRepository } from '@/domain/repositories/UserRepository';
+import { ProfileImageRepository } from '@/domain/repositories/ProfileStorageRepository';
 import { CreateUserDto } from './dto/CreateUserDto';
+import bcrypt from 'bcrypt';
+import { User } from '@/domain/entities/User';
 
 export default class CreateUserUsecase {
-  private readonly userRepository: UserRepository;
-  private readonly createUserDto: CreateUserDto;
-
-  constructor(userRepository: UserRepository, createUserDto: CreateUserDto) {
-    this.userRepository = userRepository;
-    this.createUserDto = createUserDto;
-  }
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly profileImageRepository: ProfileImageRepository
+  ) {}
 
   async execute(createUserDto: CreateUserDto): Promise<{ userId: number }> {
-    // userRepository.create는 숫자를 반환하므로 userId를 그대로 반환
-    const userId = await this.userRepository.create(this.createUserDto);
-    return { userId }; // userId를 객체로 감싸서 반환
+    const { email, password, nickname, profile_image } = createUserDto;
+
+    let fileName: string | undefined;
+
+    // 1. 이미지 파일이 있다면 Supabase에 저장
+    if (profile_image) {
+      fileName = await this.profileImageRepository.save(profile_image as File);
+    }
+
+    // 2. 비밀번호 해싱
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // 3. User 객체 생성
+    const user: User = {
+      email,
+      password: hashedPassword,
+      nickname,
+      profile_image_url: fileName,
+    };
+
+    // 4. DB에 저장하고 userId 반환
+    const userId = await this.userRepository.create(user);
+    return { userId };
   }
 }

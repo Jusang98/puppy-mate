dab1ff1fca0705e5acb3b2359b545946be95623e
devGO <been2zz@naver.com>
2025-04-22 16:21:42 +0900

    feat: StorageRepository > saveMany 생성

--- puppy-mate/domain/repositories/StorageRepository.ts
+++ puppy-mate/domain/repositories/StorageRepository.ts
@@ -1,4 +1,5 @@
 export interface StorageRepository {
   save(file: File): Promise<string>;
-  getPublicUrl(fileName: string): Promise<string> 
+  saveMany(file: File[]): Promise<string[]>;
+  getPublicUrl(fileName: string): Promise<string>;
 }

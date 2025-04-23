09739ec692e1af000788b2d76d1c416ace358344
devGO <been2zz@naver.com>
2025-04-23 16:48:22 +0900

    feat: getPostUsecase > images 추가

--- puppy-mate/application/usecases/post/GetPostUsecase.ts
+++ puppy-mate/application/usecases/post/GetPostUsecase.ts
@@ -1,16 +1,25 @@
 import { PostRepository } from '@/domain/repositories/PostRepository';
 import { PostDto } from './dto/PostDto';
 import { CoordinateRepository } from '@/domain/repositories/CoordinateRepository';
+import { PostImageRepository } from '@/domain/repositories/PostImageRepository';
+import { StorageRepository } from '@/domain/repositories/StorageRepository';
 
 export default class GetPostUsecase {
   private readonly postRepository: PostRepository;
   private readonly coordinateRepository: CoordinateRepository;
+  private readonly postImageRepository: PostImageRepository;
+  private readonly storageRepository: StorageRepository;
+
   constructor(
     postRepository: PostRepository,
-    coordinateRepository: CoordinateRepository
+    coordinateRepository: CoordinateRepository,
+    postImageRepository: PostImageRepository,
+    storageRepository: StorageRepository
   ) {
     this.postRepository = postRepository;
     this.coordinateRepository = coordinateRepository;
+    this.postImageRepository = postImageRepository;
+    this.storageRepository = storageRepository;
   }
 
   async execute(id: number): Promise<PostDto | null> {
@@ -18,9 +27,22 @@ export default class GetPostUsecase {
     if (!post) {
       return null;
     }
+
     const coordinates = await this.coordinateRepository.findAllByCourseId(
       post.courseId
     );
+
+    const images = await this.postImageRepository.findByPostId(id);
+    console.log(images);
+    // Get public URLs for all images
+    const imagesWithUrls = await Promise.all(
+      images.map(async image => ({
+        ...image,
+        url: await this.storageRepository.getPublicUrl(image.imageUrl)
+      }))
+    );
+
+    const imageUrls = imagesWithUrls.map(image => image.url);
 
     return {
       userId: post.userId,
@@ -30,7 +52,8 @@ export default class GetPostUsecase {
       content: post.content,
       createdAt: post.createdAt,
       updatedAt: post.updatedAt,
-      coordinates: coordinates
+      coordinates: coordinates,
+      images: imageUrls
     } as PostDto;
   }
 }

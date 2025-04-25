9d1ed47c7f5724c6af89c943161694ab052f7664
Jusang98 <rlatkdwn3379@gmail.com>
2025-04-25 14:08:05 +0900

    feat: GetMyCourses 유스케이스, dto 에 id 필드 추가

--- puppy-mate/application/usecases/course/dto/GetMyCoursesDto.ts
+++ puppy-mate/application/usecases/course/dto/GetMyCoursesDto.ts
@@ -1,12 +1,12 @@
-import { CoordinateDto } from './CoordinateDto';
 export class GetMyCoursesDto {
   constructor(
+    public id: number, // ← 추가
     public name: string,
     public address: string,
     public distance: number,
     public duration: number,
-    public coordinates: CoordinateDto[],
-    public isPublic?: boolean,
-    public createdAt?: Date
+    public coordinates: { lat: number; lng: number }[],
+    public isPublic: boolean,
+    public createdAt: Date
   ) {}
 }

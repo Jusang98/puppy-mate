3602b68de7952e3252574d21fc940482e3a22a14
Jusang98 <rlatkdwn3379@gmail.com>
2025-04-28 12:14:33 +0900

    route api 수정

--- puppy-mate/app/api/posts/route.ts
+++ puppy-mate/app/api/posts/route.ts
@@ -3,15 +3,15 @@ import CreatePostUsecase from '@/application/usecases/post/CreatePostUsecase';
 import { SbCourseRepository } from '@/infra/repositories/supabase/SbCourseRepository';
 import { SbPostRepository } from '@/infra/repositories/supabase/SbPostRepository';
 import { NextRequest, NextResponse } from 'next/server';
-
 import { SbPostImageRepository } from '@/infra/repositories/supabase/SbPostImageRepository';
 import { SbStorageRepository } from '@/infra/repositories/supabase/SbStorageRepository';
 import GetPostsByCourseIdUsecase from '@/application/usecases/post/GetPostsByCourseIdUsecase';
 import { SbCoordinatesRepository } from '@/infra/repositories/supabase/SbCoordinatesRepository';
 import { getUserIdFromRequest } from '@/utils/auth';
+
 export const dynamic = 'force-dynamic';
-// 코스 아이디로 게시물 조회
-// 게시물의 정보와 코스의 좌표 정보를 함께 반환
+
+// [GET] /api/post?courseId=123
 export async function GET(request: NextRequest) {
   try {
     const searchParams = request.nextUrl.searchParams;
@@ -24,7 +24,7 @@ export async function GET(request: NextRequest) {
       );
     }
 
-    const courseId = parseInt(courseIdParam);
+    const courseId = parseInt(courseIdParam, 10);
     if (isNaN(courseId)) {
       return NextResponse.json({ error: 'Invalid course ID' }, { status: 422 });
     }
@@ -36,10 +36,7 @@ export async function GET(request: NextRequest) {
     );
     const posts = await getPostsByCourseIdUsecase.execute(courseId);
 
-    if (!posts || posts.length === 0) {
-      return NextResponse.json(posts, { status: 200 });
-    }
-
+    // posts가 없거나 빈 배열이어도 200 OK로 빈 배열 반환
     return NextResponse.json(posts, { status: 200 });
   } catch (error) {
     console.error('Error getting posts by course ID:', error);
@@ -50,17 +47,24 @@ export async function GET(request: NextRequest) {
   }
 }
 
+// [POST] /api/post
 export async function POST(request: NextRequest) {
   try {
     const formData = await request.formData();
     const userId = getUserIdFromRequest(request);
+
     if (!userId) {
       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
+
     const courseId = Number(formData.get('courseId'));
     const title = formData.get('title') as string;
     const content = formData.get('content') as string;
     const images = formData.getAll('images') as File[];
+
+    if (!courseId || !title) {
+      return NextResponse.json({ error: 'Invalid request' }, { status: 422 });
+    }
 
     const createPostDto = new CreatePostDto(
       userId,
@@ -76,6 +80,7 @@ export async function POST(request: NextRequest) {
       new SbStorageRepository()
     );
     const newPostId = await createPostUsecase.execute(createPostDto);
+
     return NextResponse.json(
       { message: '게시물 작성이 완료되었습니다.', newPostId },
       { status: 201 }

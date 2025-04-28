6f3281f31d745f9f3e62421a7e72eeebbb81a00c
Jusang98 <rlatkdwn3379@gmail.com>
2025-04-28 11:36:23 +0900

    fix: 뒤로가기 버튼 기능 수정

--- puppy-mate/app/(user)/mypage/components/Mypageheader.tsx
+++ puppy-mate/app/(user)/mypage/components/Mypageheader.tsx
@@ -9,22 +9,19 @@ export default function Mypageheader({ title }: MypageheaderProps) {
   const router = useRouter();
 
   const handleBackBtnClick = () => {
-    router.back();
+    router.push('/mypage');
   };
 
   return (
-    <div className="h-[60px] relative p-4 bg-white flex items-center justify-center border-b">
+    <div className='h-[60px] relative p-4 bg-white flex items-center justify-center border-b'>
       <button
         onClick={handleBackBtnClick}
-        className="absolute left-4 p-1"
-        aria-label="뒤로가기"
+        className='absolute left-4 p-1'
+        aria-label='뒤로가기'
       >
         <IoChevronBackOutline size={24} />
       </button>
-      {title &&
-        <h1 className="text-lg font-semibold">
-          {title}
-        </h1>}
+      {title && <h1 className='text-lg font-semibold'>{title}</h1>}
     </div>
   );
 }
